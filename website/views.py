557fdae5e4b2b1225b50a5d45914ccdee32eca35
Kristina <kristina.kornilova.post@gmail.com>
2024-01-25 22:34:23 +0200

    add search page func

--- website/views.py
+++ website/views.py
@@ -5,6 +5,8 @@ from dotenv import load_dotenv
 
 import requests
 
+import json
+
 load_dotenv()
 
 
@@ -13,7 +15,7 @@ def index(request):
     API_KEY = os.getenv("API_KEY")
 
     params = {"apiKey": API_KEY,
-              "cuisine": "italian, greek",
+              #   "cuisine": "italian, greek",
               "number": "3"}
 
     response = requests.get(URL, params=params).json()
@@ -22,4 +24,50 @@ def index(request):
 
 
 def search_all_recipes(request):
-    return render(request, 'website/search_all_recipes.html')
+    meal_types = ["main course", "side dish", "dessert", "appetizer", "salad", "bread", "breakfast",
+                  "soup", "beverage", "sauce", "marinade", "fingerfood", "snack", "drink"]
+
+    cuisines = ["African", "Asian", "American", "British", "Cajun",
+                "Caribbean", "Chinese", "Eastern European", "European"]
+
+    diet = ["Gluten Free", "Ketogenic",
+            "Vegetarian", "Lacto-Vegetarian", "Vegan"]
+    intolerance = ["Dairy", "Egg", "Gluten", "Grain", "Peanut", "Seafood",
+                   "Sesame", "Shellfish", "Soy", "Sulfite", "Tree Nut", "Wheat"]
+
+    # URL_RANDOM = os.getenv("URL_RANDOM")
+    # API_KEY = os.getenv("API_KEY")
+
+    # params = {"apiKey": API_KEY,
+    #           "number": "2"}
+
+    # response = requests.get(URL_RANDOM, params=params).json()
+    # # print(response)
+    # recipes = response["recipes"]
+    with open("recipes1.txt") as file:
+        data = file.read()
+        result = json.loads(data)
+        recipes = result["recipes"]
+        recipes_details = []
+
+        for recipe in recipes:
+            recipe_detail = {"title": recipe["title"],
+                             "image": recipe["image"],
+                             "ready_in_minutes": recipe["readyInMinutes"],
+                             "servings": recipe["servings"],
+                             "ingredients": recipe["extendedIngredients"]
+                             }
+            recipes_details.append(recipe_detail)
+
+        for recipe in recipes_details:
+            ingredients_details = []
+            for ingredient in recipe["ingredients"]:
+                ingredients_details.append(
+                    {ingredient["nameClean"]: {ingredient["amount"]: ingredient["unit"]}})
+            recipe["ingredients"] = ingredients_details
+
+    return render(request, 'website/search_all_recipes.html', {"meal_types": meal_types,
+                                                               "cuisines": cuisines,
+                                                               "diet": diet,
+                                                               "intolerance": intolerance,
+                                                               "recipes": recipes_details})
